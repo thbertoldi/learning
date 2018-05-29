eac214722134f44b46ca959081bfda7689134f64
Thiago Bertoldi <thb96@hotmail.com>
2018-05-29 15:43:09 -0300

    Chapter 10
    Signed-off-by: Thiago Bertoldi <thb96@hotmail.com>

--- flask/config.py
+++ flask/config.py
@@ -12,4 +12,5 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['thiago.bertoldi@khomp.com']
+    POSTS_PER_PAGE = 3
     
